ace7d15ed280a6f34e74c5f71ce35bfae25f0f7d
N-z0 <syslog@laposte.net>
2022-02-18 16:32:35 +0700

    upgrade tags compiler
    Because tags organisation changed.
    We need to change the tags compiler (it is also an opportunity to upgrade it)

--- tools/compile-tags.py
+++ tools/compile-tags.py
@@ -1,36 +1,25 @@
 #!/usr/bin/env python3
 
+### N-z0
+### 2021
+### v2
+### the all-tags.txt files are created by the compilation of all tags found in each json directories.
+ 
 
 import json
 #import sys
 import os
 from os import path
 
-JSON_FILES="../data/tags/templates"
-OUTPUT_FILE="../data/tags/args.txt"
+JSON_FILES="../data/tags/"
+OUTPUT_FILE="all-tags.txt"
 JSON_EXT='.json'
 
 
-def get_files(root_path=".",exclude_prefixes=('__', '.')):
-	db=[]
-	for root, dirs, files in os.walk(root_path):
-		# exclude all dirs starting with exclude_prefixes
-		dirs[:] = [d for d in dirs if not d.startswith(exclude_prefixes)]
-		files = [f for f in files if not f.startswith(exclude_prefixes)]
-		for name in files :
-			if path.splitext(name)[1]==JSON_EXT :
-				pathfile=os.path.join(root,name)
-				db.append(pathfile)
-	return db
-
-
-def get_keys(files_list):
-	db=set([])
-	for p in files_list :
-		with open(p, 'r') as f :
-			dico = json.load(f)[0]
-			db.update( dico.keys() )
-	return db
+def get_keys(pathfile):
+	with open(pathfile, 'r') as f :
+		dico = json.load(f)[0]
+	return dico.keys()
 
 
 def write_args(db,output_file):
@@ -43,10 +32,19 @@ def write_args(db,output_file):
 if __name__ == '__main__' :
 	progpath= path.dirname(__file__)
 	jsonpath= path.normpath(path.join(progpath,JSON_FILES))
-	outfile= path.normpath(path.join(progpath,OUTPUT_FILE))
-	#print( __file__, progpath, jsonpath, outfile )
-
-	fl=get_files(jsonpath)
-	db=get_keys(fl)
-	write_args(db,outfile)
+	#print( __file__, progpath, jsonpath )
+	
+	for elements in os.walk(jsonpath) :
+		directory=elements[0]
+		files=elements[2]
+		db=set([])
+		for name in files :
+			if path.splitext(name)[1]==JSON_EXT :
+				pathfile=os.path.join(directory,name)
+				print( "get: ",pathfile )
+				db.update( get_keys(pathfile) )
+		if db :
+			outfile= path.join(directory,OUTPUT_FILE)
+			print( "write: ",outfile )
+			write_args(db,outfile)
 
